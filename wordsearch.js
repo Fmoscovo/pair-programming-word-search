d2e9fe8d54fafeedd4f2180e903540198b3d1d4b
Filipe Moscovo <your@email.com>
2023-07-05 14:31:06 -0600

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  let reversedArray = word.split("");
+  reversedArray = reversedArray.reverse();
 
-module.exports = wordSearch
+  const reversedWord = reversedArray.join("");
+
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  const transposedLetters = letters[0].map((col, i) =>
+    letters.map((row) => row[i])
+  );
+  const verticalJoin = transposedLetters.map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (l of horizontalJoin) {
+    if (l.includes(reversedWord)) return true;
+  }
+  for (l of verticalJoin) {
+    if (l.includes(reversedWord)) return true;
+  }
+  return false;
+};
+
+const wordArray = [
+  ["a", "b", "c", "d"],
+  ["c", "f", "g", "h"],
+  ["e", "z", "s", "t"],
+];
+console.log(wordSearch(wordArray, ""));
+
+module.exports = wordSearch;
